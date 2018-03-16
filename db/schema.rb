bd483096ee3bc98f6c299cce05413e1ae3db1a20
Katie Ediger <katieediger@gmail.com>
2018-03-16 14:24:28 -0500

    Migrated to database with $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180316171749) do
+ActiveRecord::Schema.define(version: 20180316192143) do
 
   create_table "reservations", force: :cascade do |t|
     t.string "date"
     t.string "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "rooms", force: :cascade do |t|
